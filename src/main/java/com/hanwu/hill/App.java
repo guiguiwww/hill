60dcc4074064c5dbaca99f0794bcd0c6c53fa0d8
vpersie9 <834538028@qq.com>
2018-11-19 22:49:00 +0800

    adpate the Response bean defined to bean.xml way.

--- src/main/java/com/hanwu/hill/App.java
+++ src/main/java/com/hanwu/hill/App.java
@@ -1,11 +1,10 @@
 package com.hanwu.hill;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.hanwu.hill.dao")//将项目中对应的mapper类的路径加进来就可以了
+//@MapperScan("com.hanwu.hill.dao")//将项目中对应的mapper类的路径加进来就可以了
 public class App {
 
     public static void main(String[] args) {
--- src/main/java/com/hanwu/hill/config/ConfigClass.java
+++ src/main/java/com/hanwu/hill/config/ConfigClass.java
@@ -1,10 +1,18 @@
 package com.hanwu.hill.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
+/**
+ * MapperScan could also be assigned under App.java.
+ * However it could be better under the ConfigClass, which could manage the
+ * configuration like the bean.xml and dao mapper. 
+ */
+
 @Configuration
 @ImportResource(locations={"classpath:application-bean.xml"})
+@MapperScan("com.hanwu.hill.dao")
 public class ConfigClass {
     
 }
