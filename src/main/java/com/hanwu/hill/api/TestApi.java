43d804b4f60c2187f0a9590a4b92b934bc11f01b
vpersie9 <834538028@qq.com>
2019-10-02 12:22:43 +0800

    add stub in test api

--- src/main/java/com/hanwu/hill/api/TestApi.java
+++ src/main/java/com/hanwu/hill/api/TestApi.java
@@ -6,9 +6,12 @@ import java.util.List;
 import com.hanwu.hill.response.ApiResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * TestApi.java
@@ -16,16 +19,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author Stefan
  */
 @Controller
-@RequestMapping(value="/test")
+@RequestMapping(value="/test/api")
 public class TestApi {
 
     @ResponseBody
-    @GetMapping(value = "/api")
+    @GetMapping(value = "/get.json")
     public ApiResponse<?> testGetApi() {
         List<String> testList = new ArrayList<>();
         testList.add("test1");
         testList.add("test2");
         testList.add("test3");
         return ApiResponse.triggerSuccess(testList);
+    }
+
+    @ResponseBody
+    @PutMapping(value = "/put.json")
+    public ApiResponse<?> testPutApi() {
+        // TODO
+        return null;
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/post.json")
+    public ApiResponse<?> testPostApi() {
+        // TODO
+        return null;
+    }
+
+    @ResponseBody
+    @DeleteMapping(value = "/delete.json")
+    public ApiResponse<?> testDeleteApi() {
+        // TODO
+        return null;
     }
 }
