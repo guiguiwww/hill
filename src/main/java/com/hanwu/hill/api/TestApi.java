9b711d6a476c9a2a6125f762d25cbcfc98b64e6f
vpersie9 <834538028@qq.com>
2019-10-03 19:04:38 +0800

    update request

--- src/main/java/com/hanwu/hill/api/TestApi.java
+++ src/main/java/com/hanwu/hill/api/TestApi.java
@@ -3,6 +3,7 @@ package com.hanwu.hill.api;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.alibaba.fastjson.JSONObject;
 import com.hanwu.hill.response.ApiResponse;
 
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,30 +28,39 @@ public class TestApi {
     @GetMapping(value = "/get.json")
     public ApiResponse<?> testGetApi() {
         List<String> testList = new ArrayList<>();
-        testList.add("test1");
-        testList.add("test2");
-        testList.add("test3");
+        testList.add("get1");
+        testList.add("get2");
+        testList.add("get3");
         return ApiResponse.triggerSuccess(testList);
     }
 
     @ResponseBody
     @PutMapping(value = "/put.json")
-    public ApiResponse<?> testPutApi() {
-        // TODO
-        return null;
+    public ApiResponse<?> testPutApi(@RequestBody JSONObject json) {
+        List<String> testList = new ArrayList<>();
+        testList.add("put1");
+        testList.add("put2");
+        testList.add("put3");
+        return ApiResponse.triggerSuccess(testList);
     }
 
     @ResponseBody
     @PostMapping(value = "/post.json")
-    public ApiResponse<?> testPostApi() {
-        // TODO
-        return null;
+    public ApiResponse<?> testPostApi(@RequestBody JSONObject json) {
+        List<String> testList = new ArrayList<>();
+        testList.add("post1");
+        testList.add("post2");
+        testList.add("post3");
+        return ApiResponse.triggerSuccess(testList);
     }
 
     @ResponseBody
     @DeleteMapping(value = "/delete.json")
-    public ApiResponse<?> testDeleteApi() {
-        // TODO
-        return null;
+    public ApiResponse<?> testDeleteApi(@RequestBody JSONObject json) {
+        List<String> testList = new ArrayList<>();
+        testList.add("delete1");
+        testList.add("delete2");
+        testList.add("delete3");
+        return ApiResponse.triggerSuccess(testList);
     }
 }
